88edb4f1009958e6863c1fc56f138353cfe30ab9
rafap <cachera2112@gmail.com>
2026-09-08 21:58:51 -0300

    feat: adiciona provider do next-themes e configura dark mode do tailwind

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import ThemeProvider from './components/ThemeProvider';
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,9 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
-      <body className={`${inter.className} antialiased bg-neutral-950 text-neutral-50`}>
-        {children}
+    // Adicionamos suppressHydrationWarning para o next-themes funcionar perfeitamente com a tag html
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50 min-h-screen transition-colors`}>
+        <ThemeProvider>
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
